02dc47fed0698479f7bcafa9b235a9f0feb37469
Віталій Лакатош <some.pochta1@gmail.com>
2021-01-09 23:40:47 +0200

    DIsplay sad face on Matrix 8x8

---
+++ displayEmoji.ino
@@ -0,0 +1,60 @@
+/// <summary>
+/// Use matrix 8x8.
+/// </summary>
+// <copyright file="max8x8.ino" company="lakatoshvv">
+// Copyright (c) lakatoshvv. All rights reserved.
+// </copyright>
+
+#include "LedControl.h"
+#include "binary.h"
+
+const int DIN_PIN = 12;
+const int CLK_PIN = 11;
+const int CS_PIN = 10;
+
+LedControl lc=LedControl(DIN_PIN, CLK_PIN, CS_PIN,1);
+
+void setup() {
+  lc.shutdown(0,false);
+  lc.setIntensity(0,8);
+  lc.clearDisplay(0);  
+}
+
+void drawFaces(){
+  // Showing a sad face:
+  lc.setRow(0,0,sf[0]);
+  lc.setRow(0,1,sf[1]);
+  lc.setRow(0,2,sf[2]);
+  lc.setRow(0,3,sf[3]);
+  lc.setRow(0,4,sf[4]);
+  lc.setRow(0,5,sf[5]);
+  lc.setRow(0,6,sf[6]);
+  lc.setRow(0,7,sf[7]);
+  delay(delaytime);
+  
+  // показываем «покерфейс»:
+  lc.setRow(0,0,nf[0]);
+  lc.setRow(0,1,nf[1]);
+  lc.setRow(0,2,nf[2]);
+  lc.setRow(0,3,nf[3]);
+  lc.setRow(0,4,nf[4]);
+  lc.setRow(0,5,nf[5]);
+  lc.setRow(0,6,nf[6]);
+  lc.setRow(0,7,nf[7]);
+  delay(delaytime);
+  
+  // показываем веселую рожицу:
+  lc.setRow(0,0,hf[0]);
+  lc.setRow(0,1,hf[1]);
+  lc.setRow(0,2,hf[2]);
+  lc.setRow(0,3,hf[3]);
+  lc.setRow(0,4,hf[4]);
+  lc.setRow(0,5,hf[5]);
+  lc.setRow(0,6,hf[6]);
+  lc.setRow(0,7,hf[7]);
+  delay(delaytime);
+}
+
+void loop(){
+  drawFaces();
+}

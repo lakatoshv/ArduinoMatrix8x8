753ed9dbf9ebe00cd35931a8bbfc556a70159b33
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-09-19 21:28:52 +0300

    Display symbols

--- displaySymbols.ino
+++ displaySymbols.ino
@@ -128,6 +128,7 @@ char string2[] = " A B C D E F G H I J K L M N O P Q R S T U V X W Y Z ";
 char string3[] = " a b c d e f g h i j k l m n o p q r s t u v x w y z ";
 char string4[] = " 1 2 3 4 5 6 7 8 9 0 - = ";
 char string5[] = " ! @ # $ % ¨ & * ( ) _ + ";
+char string6[] = " ' , . ; ~ ] ´ [ | < > : ^ } ` { / ?  ";
 
 
 void setup(){

882f30038173bd5c8c0a19923cf170c9c70daa19
Vitalii Lakatosh <some.pochta1@gmail.com>
2022-09-19 21:29:14 +0300

    Display text

--- displaySymbols.ino
+++ displaySymbols.ino
@@ -129,6 +129,7 @@ char string3[] = " a b c d e f g h i j k l m n o p q r s t u v x w y z ";
 char string4[] = " 1 2 3 4 5 6 7 8 9 0 - = ";
 char string5[] = " ! @ # $ % ¨ & * ( ) _ + ";
 char string6[] = " ' , . ; ~ ] ´ [ | < > : ^ } ` { / ?  ";
+char heipaadeg[] = "Hello World";
 
 
 void setup(){
